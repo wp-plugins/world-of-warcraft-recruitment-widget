a9dff21409e0ca807e1323be36930b7faf2b8bcd
yanitor <yanitor@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-28 13:23:59 +0000

    Adding first version of my plugin
    git-svn-id: https://plugins.svn.wordpress.org/world-of-warcraft-recruitment-widget/trunk@1077522 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,35 @@
+=== World of Warcraft Recruitment Widget ===
+Contributors: yanitor
+Donate link:
+Tags: world of warcaft, wow, warcraft, recruitment, 
+Requires at least: 4.1
+Tested up to: 4.1
+Stable tag: 1.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+A easy to use WoW Recruitmet Widget
+
+== Description ==
+
+A easy to use WoW Recruitmet Widget
+
+== Installation ==
+
+1. Upload the `world-of-warcraft-recruitment-widget` directory to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. Setup the widget in the widgets settings site in your Wordpress admin area
+
+== Screenshots ==
+
+1. Screenshot 2015-01-26 13.21.53.png.
+
+== Changelog ==
+
+= 1.1 =
+* Some Code Tweaks
+* Added Screenshot
+* Wider optionspanel
+
+= 1.0 =
+* Create this plugin
---
+++ world-of-warcraft-recruitment-widget.php
@@ -0,0 +1,141 @@
+<?php
+/*
+Plugin Name: World of Warcraft Recruitment Widget
+Plugin URI:
+Description: A simple to use WoW Recruitment Widget Plugin
+Version: 1.1
+Author: Yan Paiha
+Author URI:
+License: GPL2
+*/
+/*
+Copyright 2015  Yan Pauha  (email : y.paiha©gmail.com)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License, version 2, as
+published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+/**
+ * Adds WoW Recruitment widget
+ */
+class World_of_Warcraft_recruitment_widget extends WP_Widget
+{
+
+	private $classes = [
+		'Warrior'    => ['Arms', 'Fury', 'Protection'],
+		'Paladin'    => ['Holy', 'Protection', 'Retribution'],
+		'Hunter'     => ['Beastmaster', 'Marksman', 'Survival'],
+		'Rogue'      => ['Assassination', 'Combat', 'Subtlety'],
+		'Priest'     => ['Discipline', 'Holy', 'Shadow'],
+		'Deathknigh' => ['Blood', 'Frost', 'Unholy'],
+		'Shaman'     => ['Elemental', 'Enhancement', 'Restoration'],
+		'Mage'       => ['Arcane', 'Fire', 'Frost'],
+		'Warlock'    => ['Affliction', 'Demonology', 'Destruction'],
+		'Monk'       => ['Brewmaster', 'Mistweaver', 'Battledancer'],
+		'Druid'      => ['Balance', 'Cat', 'Bear', 'Restoration'],
+	];
+
+	public function __construct()
+	{
+		$widget_ops = ['description' => __( 'A simple to use WoW Recruitment Widget', 'world_of_warcraft_recruitment_widget_textdomain' )];
+		$control_ops = ['width' => 500];
+		
+		parent::__construct('world_of_warcraft_recruitment_widget', __( 'World of Warcraft recruitment widget', 'world_of_warcraft_recruitment_widget_textdomain' ), $widget_ops, $control_ops);
+
+	}
+
+	public function widget($args, $instance)
+	{
+		echo $args['before_widget'];
+		if ( ! empty( $instance['title'] ) ) {
+			_e( $args['before_title'].apply_filters( 'widget_title', $instance['title'] ).$args['after_title'] );
+		}
+
+		?>
+		<table class="recruitment_table">
+		<?php foreach ( $this->classes as $key => $value ): ?>
+
+			<?php if ( 'on' == $instance[ $key ] ): ?>
+				<tr class="<?php echo $key ?>">
+					<td><?php _e( $key, 'world_of_warcraft_recruitment_widget_textdomain' ) ?></td>
+					<td>
+						<?php foreach ( $value as $spec ): ?>
+							<?php $style = ( $instance[ $key.'_'.$spec ] == 'on' ? 'open' : 'close' ); ?>
+							<?php echo '<img class="'.$style.'" src="'.plugins_url( '/img/wow_icons/'.$key.'_'.$spec, __FILE__ ).'.jpg" > '; ?>
+						<?php endforeach ?>
+					</td>
+				</tr>
+			<?php endif ?>
+
+		<?php endforeach ?>
+		</table>
+
+		<?php
+
+		echo $args['after_widget'];
+	}
+
+	public function form($instance)
+	{
+		$title = ! empty($instance['title']) ? $instance['title'] : __( 'New Title', 'world_of_warcraft_recruitment_widget_textdomain' );
+		?>
+
+		<p>
+		<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:', 'world_of_warcraft_recruitment_widget_textdomain' ); ?></label>
+		<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>">
+		</p>
+		
+		<?php foreach ( $this->classes as $key => $value ): ?>
+			<p>
+				<label for="<?php echo $this->get_field_id( $key ) ?>"><?php _e( $key, 'world_of_warcraft_recruitment_widget_textdomain' ) ?></label>
+				<input type="checkbox" name="<?php echo $this->get_field_name( $key ) ?>" id="<?php echo $this->get_field_id( $key ) ?>" <?php checked( $instance[ $key ], 'on' ); ?>> Show
+				<?php foreach ( $value as $spec ): ?>
+					<input type="checkbox" name="<?php echo $this->get_field_name( $key.'_'.$spec ) ?>" id="<?php echo $this->get_field_id( $key.'_'.$spec ) ?>" <?php checked( $instance[ $key.'_'.$spec ], 'on' ); ?>> <?php _e( $spec, 'world_of_warcraft_recruitment_widget_textdomain' ) ?>
+				<?php endforeach ?>
+			</p>
+		<?php endforeach ?>
+
+	<?php
+
+	}
+
+	public function update($new_instance, $old_instance)
+	{
+		$instance = array();
+		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+
+		foreach ( $this->classes as $key => $value ) {
+			$instance[ $key ] = $new_instance[ $key ];
+
+			foreach ( $value as $spec ) {
+				$instance[ $key.'_'.$spec ] = $new_instance[ $key.'_'.$spec ];
+			}
+		}
+
+		return $instance;
+	}
+}
+
+function add_style_files()
+{
+	wp_enqueue_style( 'widget_style', plugins_url( 'style.css', __FILE__ ), false, '1.0.0', 'all' );
+}
+add_action( 'wp_enqueue_scripts', 'add_style_files' );
+
+function register_world_of_warcraft_recruitment_widget()
+{
+	register_widget( 'World_of_Warcraft_recruitment_widget' );
+}
+
+add_action( 'widgets_init', 'register_world_of_warcraft_recruitment_widget' );
+load_plugin_textdomain( 'world_of_warcraft_recruitment_widget_textdomain', false, dirname( plugin_basename( __FILE__ ) ).'/languages' );
